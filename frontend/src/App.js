783035941d32e57998ed6e4f2ba3406cbbca709b
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2021-10-12 21:06:10 +0700

    add card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Button } from "./components/atoms/button";
+import { Card } from "./components/atoms/card";
 import { Title } from "./components/atoms/title";
 import "./style.scss";
 function App() {
@@ -6,6 +7,10 @@ function App() {
     <div className="app">
       <Title title="To-Do List App" />
       <Button label="click!" />
+      <Card>
+        <Button label="card button" />
+      </Card>
+
     </div>
   );
 }
